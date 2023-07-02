6d8c6b520ca72238a755f2faa77b741d2781cfe4
Abu Fahim <ivanf.if06@gmail.com>
2023-07-02 22:46:04 +0700

    edit about me

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const url = 'https://www.forestapi.my.id/api/instagram/user/ivanfsyh';
 fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+        // document.getElementById('img-ig').src = data['image_url'];
+        // // document.getElementById('username').innerHTML =
+        // //     '<i class="fa-brands fa-instagram"></i> ' + data['username'];
+        console.log(data);
+    })
     .catch((error) => console.log(error));
